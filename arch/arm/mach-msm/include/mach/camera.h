b5d984fa56e1f38c9861e362537bd16511fdaffa
Dima Zavin <dima@android.com>
2010-07-27 15:08:13 -0700

    [ARM] media: video: msm: Don't force V4L2 on when using MSM camera
    Currently, the primary MSM camera driver does not rely on V4L2.
    A V4L2 interface does exist, but is very minimal and is not well tested.
    Hence, we add a separate config option for enabling a V4L2 driver interface,
    and allow the main MSM camera code to be compiled without V4L2.
    
    Change-Id: Idc01e75a42c1906d8cd4eff757a15b7c0d942f69
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/include/mach/camera.h
+++ arch/arm/mach-msm/include/mach/camera.h
@@ -255,6 +255,7 @@ struct axidata {
 	}
 #endif
 
+#ifdef CONFIG_MSM_CAMERA_V4L2
 /* Below functions are added for V4L2 kernel APIs */
 struct msm_v4l2_driver {
 	struct msm_sync *sync;
@@ -271,6 +272,7 @@ struct msm_v4l2_driver {
 
 int msm_v4l2_register(struct msm_v4l2_driver *);
 int msm_v4l2_unregister(struct msm_v4l2_driver *);
+#endif
 
 void msm_camvfe_init(void);
 int msm_camvfe_check(void *);
--- drivers/media/video/msm/msm_camera-7x30.c
+++ drivers/media/video/msm/msm_camera-7x30.c
@@ -1010,6 +1010,7 @@ static int msm_get_stats(struct msm_sync *sync, void __user *arg)
 		se.ctrl_cmd.resp_fd = ctrl->resp_fd;
 		break;
 
+#ifdef CONFIG_MSM_CAMERA_V4L2
 	case MSM_CAM_Q_V4L2_REQ:
 		/* control command from v4l2 client */
 		ctrl = (struct msm_ctrl_cmd *)(qcmd->command);
@@ -1032,7 +1033,7 @@ static int msm_get_stats(struct msm_sync *sync, void __user *arg)
 		se.ctrl_cmd.type   = ctrl->type;
 		se.ctrl_cmd.length = ctrl->length;
 		break;
-
+#endif
 	default:
 		rc = -EFAULT;
 		goto failure;
@@ -2436,6 +2437,7 @@ static int msm_open_control(struct inode *inode, struct file *filep)
 	return rc;
 }
 
+#ifdef CONFIG_MSM_CAMERA_V4L2
 static int __msm_v4l2_control(struct msm_sync *sync,
 		struct msm_ctrl_cmd *out)
 {
@@ -2480,6 +2482,7 @@ end:
 	CDBG("%s: rc %d\n", __func__, rc);
 	return rc;
 }
+#endif
 
 static const struct file_operations msm_fops_config = {
 	.owner = THIS_MODULE,
@@ -2707,7 +2710,7 @@ error:
 	return ret;
 }
 
-
+#ifdef CONFIG_MSM_CAMERA_V4L2
 int msm_v4l2_register(struct msm_v4l2_driver *drv)
 {
 	/* FIXME: support multiple sensors */
@@ -2734,6 +2737,7 @@ int msm_v4l2_unregister(struct msm_v4l2_driver *drv)
 	return 0;
 }
 EXPORT_SYMBOL(msm_v4l2_unregister);
+#endif
 
 static int msm_sync_init(struct msm_sync *sync,
 		struct platform_device *pdev,
