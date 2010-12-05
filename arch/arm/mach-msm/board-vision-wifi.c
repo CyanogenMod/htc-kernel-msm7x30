e33e66de11f52ebe5057988ae71f4b6df1034362
Steve Kondik <shade@chemlab.org>
2010-12-05 02:52:59 -0500

    bcm4329: Disable unsupported HB_OOB and KEEP_ALIVE

--- arch/arm/mach-msm/board-vision-wifi.c
+++ arch/arm/mach-msm/board-vision-wifi.c
@@ -77,7 +77,7 @@ static struct resource vision_wifi_resources[] = {
 		.name		= "bcm4329_wlan_irq",
 		.start		= MSM_GPIO_TO_INT(VISION_GPIO_WIFI_IRQ),
 		.end		= MSM_GPIO_TO_INT(VISION_GPIO_WIFI_IRQ),
-		.flags          = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE,
+		.flags          = IORESOURCE_IRQ | IORESOURCE_IRQ_LOWEDGE,
 	},
 };
 
