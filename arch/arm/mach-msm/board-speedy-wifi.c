3de83a95bed7e0db52ff9da3c455469a1c73ec2b
toastcfh <toastcfh@gmail.com>
2011-05-22 11:32:47 -0400

    speedy: wifi: use CONFIG_BCM4329_PURE_ANDROID in board

--- arch/arm/mach-msm/board-speedy-wifi.c
+++ arch/arm/mach-msm/board-speedy-wifi.c
@@ -9,7 +9,11 @@
 #include <asm/gpio.h>
 #include <asm/io.h>
 #include <linux/skbuff.h>
+#ifdef CONFIG_BCM4329_PURE_ANDROID
+#include <linux/wlan_plat.h>
+#else
 #include <linux/wifi_tiwlan.h>
+#endif
 
 #include "board-speedy.h"
 
@@ -77,7 +81,11 @@ static struct resource speedy_wifi_resources[] = {
 		.name		= "bcm4329_wlan_irq",
 		.start		= MSM_GPIO_TO_INT(SPEEDY_GPIO_WIFI_IRQ),
 		.end		= MSM_GPIO_TO_INT(SPEEDY_GPIO_WIFI_IRQ),
+#ifdef CONFIG_BCM4329_PURE_ANDROID
+		.flags		= IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE,
+#else
 		.flags          = IORESOURCE_IRQ | IORESOURCE_IRQ_LOWEDGE,
+#endif
 	},
 };
 
@@ -86,7 +94,10 @@ static struct wifi_platform_data speedy_wifi_control = {
 	.set_reset      = speedy_wifi_reset,
 	.set_carddetect = speedy_wifi_set_carddetect,
 	.mem_prealloc   = speedy_wifi_mem_prealloc,
-//	.dot11n_enable  = 1,
+#ifndef CONFIG_BCM4329_PURE_ANDROID
+	.dot11n_enable  = 1,
+	.cscan_enable   = 1,
+#endif
 };
 
 static struct platform_device speedy_wifi_device = {
