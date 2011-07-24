9bcc24c584e264dfdee3f445a6beae2da7631fe4
cayniarb <charlton.danielk@gmail.com>
2011-07-24 05:28:44 -0400

    use pure_android for saga

--- arch/arm/mach-msm/board-saga-wifi.c
+++ arch/arm/mach-msm/board-saga-wifi.c
@@ -9,7 +9,11 @@
 #include <asm/gpio.h>
 #include <asm/io.h>
 #include <linux/skbuff.h>
+#ifdef CONFIG_BCM4329_PURE_ANDROID
+#include <linux/wlan_plat.h>
+#else
 #include <linux/wifi_tiwlan.h>
+#endif
 
 #include "board-saga.h"
 
@@ -77,7 +81,11 @@ static struct resource saga_wifi_resources[] = {
 		.name		= "bcm4329_wlan_irq",
 		.start		= MSM_GPIO_TO_INT(SAGA_GPIO_WIFI_IRQ),
 		.end		= MSM_GPIO_TO_INT(SAGA_GPIO_WIFI_IRQ),
+#ifdef CONFIG_BCM4329_PURE_ANDROID
+		.flags		= IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE,
+#else
 		.flags          = IORESOURCE_IRQ | IORESOURCE_IRQ_LOWEDGE,
+#endif
 	},
 };
 
@@ -86,7 +94,9 @@ static struct wifi_platform_data saga_wifi_control = {
 	.set_reset      = saga_wifi_reset,
 	.set_carddetect = saga_wifi_set_carddetect,
 	.mem_prealloc   = saga_wifi_mem_prealloc,
+#ifndef CONFIG_BCM4329_PURE_ANDROID
 	.dot11n_enable  = 1,
+#endif
 	//.cscan_enable   = 1,
 };
 
@@ -138,6 +148,7 @@ int __init saga_wifi_init(void)
 	saga_wifi_update_nvs("sd_oobonly=1\n");
 	saga_wifi_update_nvs("btc_params80=0\n");
 	saga_wifi_update_nvs("btc_params6=30\n");
+	saga_wifi_update_nvs("btc_params70=0x32\n");
 	saga_init_wifi_mem();
 	ret = platform_device_register(&saga_wifi_device);
 	return ret;
