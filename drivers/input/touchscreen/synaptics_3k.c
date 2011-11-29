3ff30f0ba93e5126384f980ae0758bc93cb89e84
Giulio Cervera <giulio.cervera@gmail.com>
2011-11-29 01:41:09 +0100

    driver: ts: synaptics 3k changes for ics

--- drivers/input/touchscreen/synaptics_3k.c
+++ drivers/input/touchscreen/synaptics_3k.c
@@ -280,6 +280,7 @@ static void synaptics_ts_work_func(struct work_struct *work)
 		if (finger_pressed == 0) {
 #ifdef CONFIG_TOUCHSCREEN_COMPATIBLE_REPORT
 			input_report_abs(ts->input_dev, ABS_MT_TOUCH_MAJOR, 0);
+			input_report_key(ts->input_dev, BTN_TOUCH, 0);
 #else
 			input_report_abs(ts->input_dev, ABS_MT_AMPLITUDE, 0);
 			input_report_abs(ts->input_dev, ABS_MT_POSITION, 1 << 31);
@@ -321,6 +322,8 @@ static void synaptics_ts_work_func(struct work_struct *work)
 						finger_data[loop_i][0]);
 					input_report_abs(ts->input_dev, ABS_MT_POSITION_Y,
 						finger_data[loop_i][1]);
+					input_report_key(ts->input_dev, BTN_TOUCH,
+						finger_data[loop_i][2] ? 1 : 0);
 					input_mt_sync(ts->input_dev);
 #else
 					input_report_abs(ts->input_dev, ABS_MT_AMPLITUDE,
@@ -632,6 +635,7 @@ static int synaptics_ts_resume(struct i2c_client *client)
 
 #ifdef CONFIG_TOUCHSCREEN_COMPATIBLE_REPORT
 	input_report_abs(ts->input_dev, ABS_MT_TOUCH_MAJOR, 0);
+	input_report_key(ts->input_dev, BTN_TOUCH, 0);
 	input_sync(ts->input_dev);
 #else
 	input_report_abs(ts->input_dev, ABS_MT_AMPLITUDE, 0);
